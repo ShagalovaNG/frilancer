4466d64154d4702cd7c9417fcc0eac5c5349107e
Natalya Shagalova <nataliyshagalyalya@gmail.com>
2026-02-18 00:52:33 +0700

    Fix the test on parametrized

--- src/test/java/ru/netology/services/WorkCalcServiceTest.java
+++ src/test/java/ru/netology/services/WorkCalcServiceTest.java
@@ -2,23 +2,26 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class WorkCalcServiceTest {
-    @Test
-    public void testPrimerOne() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/primer.csv")
+    public void testPrimerOne(int expected, int income, int expenses, int threshold) {
         WorkCalcService service = new WorkCalcService();
-        int expected = 3;
-        int actual = service.workCalculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int actual = service.workCalculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testPrimerTwo() {
-        WorkCalcService service = new WorkCalcService();
-        int expected = 2;
-        int actual = service.workCalculate(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
+    //  @Test
+    // public void testPrimerTwo() {
+    //   WorkCalcService service = new WorkCalcService();
+    // int expected = 2;
+    //int actual = service.workCalculate(100_000, 60_000, 150_000);
+    // Assertions.assertEquals(expected, actual);
+    //}
 }
 
 
